9929421e28b14d34bc2439b4c5a005eabf849197
Jan Kamburg <jan@kamburg.de>
2021-09-23 11:51:04 +0200

    Initial Commit

---
+++ eu_traffic_map/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for eu_traffic_map project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eu_traffic_map.settings')
+
+application = get_wsgi_application()
