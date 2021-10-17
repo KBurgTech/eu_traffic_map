f56a83f6e092ffe2fcb091e4bfc344f9e7643315
Jan Kamburg <jan@kamburg.de>
2021-10-17 21:08:59 +0200

    On the main map view you can now see the time the data was last refreshed On the ML results page you can now see past ML results as well (Only for cycles that were successull)

--- traffic_map/views.py
+++ traffic_map/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 
-from traffic_map.models import MLPrediction
+from traffic_map.models import MLPrediction, RefreshCycle
 from traffic_map.service.update_service import get_current_cycle
 from traffic_map.ui.views import mapView
 
@@ -12,11 +12,16 @@ def main_view(request):
 
 def index_view(request):
     template = loader.get_template('Index.html')
-    context = {'map': mapView.map_view_main()}
+    context = {'map': mapView.map_view_main(), 'current_cycle': RefreshCycle.objects.filter(id=get_current_cycle())[0]}
     return HttpResponse(template.render(context, request))
 
 
-def ml_trainer_view(request):
+def ml_trainer_view(request, cycle_id):
     template = loader.get_template('MLTrainer.html')
-    context = {'data': MLPrediction.objects.filter(refresh_cycle=get_current_cycle())}
+    cycle_info = {
+        'allowed_runs': RefreshCycle.objects.filter(finished=True, status=1).values_list('id', flat=True),
+        'current': RefreshCycle.objects.filter(id=cycle_id)[0],
+        'latest': get_current_cycle()
+    }
+    context = {'data': MLPrediction.objects.filter(refresh_cycle=cycle_id), 'cycleInfo': cycle_info}
     return HttpResponse(template.render(context, request))
